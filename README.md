8235e848c2fe111c35208250437b171cf6c9a1a2
u_mulder <m264695502@gmail.com>
2017-04-22 13:32:25 +0300

    - Add output directory  - Add examples.php  - Update README.ml

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # yaml2btx
 Generating php-codes for bitrix entities from yaml config files
+
+## Usage sample
+
+```
+require_once "./yaml2btx/vendor/autoload.php";
+
+$g = new \Yaml2Btx\Generator();
+$g->parse('./yaml2btx/configs/iblock.yaml')->save('./output.php');
+
+```
---
+++ examples.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Yaml2Btx\Generator examples
+ *
+ * @author u_mulder <m264695502@gmail.com>
+ */
+
+error_reporting(E_ALL);
+require_once "./vendor/autoload.php";
+
+/* Instantiate new generator */
+$g = new \Yaml2Btx\Generator();
+
+/* Generate file from provided config `iblock.yaml` and store it as `new_iblock.php` */
+$g->parse('./configs/iblock.yaml')->save('./output/new_iblock.php');
+
+/* Generate file from provided config `iblock.yaml` and donwload it */
+//$g->parse('./bxcodegen/configs/iblock.yaml')->download(); // TODO
+
+echo 'DONE!' . PHP_EOL;
+
+
+
